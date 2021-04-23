27857db72bacc64b19ff30c2eff00476b788ce8a
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-23 18:08:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ToggleButton from './Components/ToggleButton';
+import ToggleButton from './components/ToggleButton';
 
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
